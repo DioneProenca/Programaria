c82c5f02187f195e7211b1e7d3016737724ba233
DioneProenca <dioneproencas@gmail.com>
2022-04-13 21:25:15 -0300

    js cofig alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-if(document.getElementById("nome").value != ("") && (document.getElementById("email").value != (""){
-	envie os dados
-}else{
-	peça a usuária que preencha os campos nome e email
+document.getElementById("botaoEnviar").addEventListener("click",validaFormulario)
+
+function validaFormulario(){
+ if(document.getElementById("nome").value !="" && 
+ document.getElementById("email").value != ""){ 
+ alert("Prontinho! Você receberá as novidades por email.")
+	} else{
+		alert("Por favor, preencha os campos nome e email")
+	}
 }
